910c3b876964a0f5a0a7bb24549f68ba3c562a53
charleneofficial <141934519+LorreaLadao@users.noreply.github.com>
2024-08-22 11:39:21 +0800

    first commit

---
+++ styles/krm.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+aint main() {
+// This is a C++ program that prints "Hello, World" to the console.
+std::cout <<"Hello, World!" << std::endl; // Output the text
+std::cout << "Hello, UA!" << std::endl; // Output the text
+
+return 0; // Return 0 to indicate successful execution
+}
---
+++ styles/wakwaktv.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+// This is a C++ program that prints "Hello, World" to the console.
+std::cout <<"Hello, World!" << std::endl; // Output the text
+std::cout << "Hello, UA!" << std::endl; // Output the text
+
+return 0; // Return 0 to indicate successful execution
+ }
